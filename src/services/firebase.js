627dab4d8f68063ce812586be1e701e1576b78c2
Mohith <mohith1904@gmail.com>
2025-02-17 22:38:53 +0530

    deleted user if not IITR

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,20 +7,20 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyBXap36-70Vl1SBSE9elvyKWBM9NPBim-w",
+  authDomain: "athletics-attendance.firebaseapp.com",
+  databaseURL: "https://athletics-attendance-default-rtdb.firebaseio.com",
+  projectId: "athletics-attendance",
+  storageBucket: "athletics-attendance.firebasestorage.app",
+  messagingSenderId: "822555078014",
+  appId: "1:822555078014:web:4c159f6165eea90f22172f",
+  measurementId: "G-ZTZKCHLMYS"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
 
 export default app;
